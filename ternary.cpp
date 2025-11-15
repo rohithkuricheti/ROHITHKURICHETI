829f2b1847b1ba55f45b4bbf7a535fc1be378a41
rohithkuricheti <rohithkuricheti@gmail.com>
2025-11-15 20:23:39 +0530

    ternary

---
+++ ternary.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the numbers of a and b");
+	scanf("%d%d",&a,&b);
+	//if(a>b){
+	//	printf("a is greater than b");
+	//}
+	//else{
+	//	printf("a is less than b");
+//	}
+a>b?printf("a is greater than b"):printf("a is less than b");
+	return 0;
+}
