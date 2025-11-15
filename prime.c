5130e0b0f0becbba2a5de44d33f2770e8a3e10ce
rohithkuricheti <rohithkuricheti@gmail.com>
2025-11-15 20:15:03 +0530

    prime_numbers

---
+++ prime.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i=1,n,f=0;
+	printf("enter an integer");
+	scanf("%d",&n);
+	while(i<=n){
+		if(n%i==0){
+		f=f+1;
+		}
+		i++;
+	}
+	if(f==2)
+	printf("prime");
+	else
+	printf("not a prime");
+	return 0;
+}
